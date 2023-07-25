f2275e469c64bf2ca5ceb353a213b4d9f43a1ed4
rokysf <135695631+rokysf@users.noreply.github.com>
2023-07-25 11:30:20 +0200

    Add files via upload

--- schema_tienda/queries_tienda.sql
+++ schema_tienda/queries_tienda.sql
@@ -21,4 +21,14 @@ SELECT nombre FROM producto WHERE codigo_fabricante = 2;
 SELECT producto.nombre, producto.precio, fabricante.nombre FROM producto, fabricante WHERE producto.codigo_fabricante = fabricante.codigo;
 SELECT producto.nombre, producto.precio, fabricante.nombre FROM producto, fabricante WHERE producto.codigo_fabricante = fabricante.codigo ORDER BY fabricante.nombre ASC;
 SELECT producto.codigo, producto.nombre, producto.codigo_fabricante, fabricante.nombre FROM producto, fabricante WHERE producto.codigo_fabricante = fabricante.codigo;
-SELECT producto.nombre, producto.precio, fabricante.nombre FROM producto, fabricante;
+SELECT producto.nombre, producto.precio, fabricante.nombre FROM producto, fabricante WHERE producto.codigo_fabricante = fabricante.codigo ORDER BY producto.precio ASC LIMIT 1;
+SELECT producto.nombre, producto.precio, fabricante.nombre FROM producto, fabricante WHERE producto.codigo_fabricante = fabricante.codigo ORDER BY producto.precio DESC LIMIT 1;
+SELECT producto.nombre, fabricante.nombre FROM producto, fabricante WHERE producto.codigo_fabricante = fabricante.codigo && fabricante.nombre = "lenovo";
+SELECT producto.nombre, producto.precio, fabricante.nombre FROM producto, fabricante WHERE producto.codigo_fabricante = fabricante.codigo && fabricante.nombre = "crucial" && producto.precio > 200;
+SELECT producto.nombre, fabricante.nombre FROM producto, fabricante WHERE (producto.codigo_fabricante = fabricante.codigo && fabricante.nombre = "Asus") || (producto.codigo_fabricante = fabricante.codigo && fabricante.nombre = "Hewlett-Packard") || (producto.codigo_fabricante = fabricante.codigo && fabricante.nombre = "Seagate");
+SELECT producto.nombre, fabricante.nombre FROM producto, fabricante WHERE (producto.codigo_fabricante = fabricante.codigo && fabricante.nombre IN ("Asus", "Hewlett-Packard", "Seagate"));
+SELECT producto.nombre, producto.precio, fabricante.nombre FROM producto, fabricante WHERE producto.codigo_fabricante = fabricante.codigo && RIGHT(fabricante.nombre,1) = "e";
+SELECT producto.nombre, producto.precio, fabricante.nombre FROM producto, fabricante WHERE producto.codigo_fabricante = fabricante.codigo && fabricante.nombre LIKE('%w%');
+
+
+
